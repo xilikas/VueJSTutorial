e3608ed0ee9073ce6165bfc5333dd14c5f015d8b
Fil Lawrence Luzadas <7342198+xilikas@users.noreply.github.com>
2019-03-29 00:50:47 -0400

    Vue looping over objects and arrays

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div id="app">
     <Header :title="title"/>
     <img width="25%" src="./assets/logo.png">
-    <HelloWorld msg="Hello World"/>
+    <h1>{{ hello }}</h1>
+    <ul>
+      <li v-for="(item, index) in list">{{index + 1}}: {{item}}</li>
+      <li v-for="person in people">{{person.name}}</li>
+      <li v-for="(value, key) in profile">{{key}}: {{value}}</li>
+    </ul>
+    <HelloWorld :key="person.id" v-for="person in people" msg="Hello World"/>
   </div>
 </template>
 
@@ -15,7 +21,23 @@ export default {
   data() {
     return {
       hello: "Hello World",
-      title: "Vue Movie DB"
+      title: "Vue Movie DB",
+      list: ["Penguin", "Turtle", "Red Panda"],
+      people: [
+        {
+          id: 1,
+          name: "Fil"
+        },
+        {
+          id: 2,
+          name: "Natalie"
+        }
+      ],
+      profile: {
+        name: "Fil",
+        age: "25",
+        job: "Developer"
+      }
     };
   },
   components: {
