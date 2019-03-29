ed2c063fd520e64933a7324dcb184c5c9ac3c38d
Fil Lawrence Luzadas <7342198+xilikas@users.noreply.github.com>
2019-03-29 11:09:07 -0400

    Fixed poster path using computed properties
    Styled app (global and component level)

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -7,22 +7,26 @@
 </template>
 
 <script>
-import Movie from './Movie';
+import Movie from "./Movie";
 
 export default {
   name: "MoviesList",
-  data() { // Initialize state
+  data() {
+    // Initialize state
     return {
       movies: []
-    }
+    };
   },
-  created: function() { // Lifecycle method for component
+  created: function() {
+    // Lifecycle method for component
     this.fetchData();
   },
   methods: {
     fetchData: async function() {
       try {
-        const res = await fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a303f2f5d7ca8203094d13ff904f520a');
+        const res = await fetch(
+          "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a303f2f5d7ca8203094d13ff904f520a"
+        );
         const movies = await res.json();
         this.movies = movies.results;
       } catch (e) {
@@ -37,5 +41,13 @@ export default {
 </script>
 
 <style scoped>
+ul {
+  display: grid;
+  list-style: none;
+  padding: 1rem;
+  margin: 0;
+  grid-row-gap: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+}
 </style>
 
